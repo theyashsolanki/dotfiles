21e6d397f25523c25bfadc659b6ea938fc4c89c1
theyashsolanki <solankiyash556@gmail.com>
2023-06-21 00:27:00 +0530

    updated scripts

--- script/tmux_session_switcher.sh
+++ script/tmux_session_switcher.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-tmux_session_switcher() {
-  # Use fzf with preview to select a session
-  selected_session=$(tmux ls | fzf --preview 'tmux list-windows -t {1} -F "#I:#W (#{session_width}x#{session_height}) - #{window_panes} panes - #{?window_active,active,}"')
 
-  if [[ ! -n $selected_session ]]; then
-    return
-  fi
+# Use fzf with preview to select a session
+selected_session=$(tmux ls | fzf --preview 'tmux list-windows -t {1} -F "#I:#W (#{session_width}x#{session_height}) - #{window_panes} panes - #{?window_active,active,}"')
 
-  # Switch to the selected session
-  if [[ -z $TMUX ]] && [[ -n "$selected_session" ]]; then # If not in tmux
-    BUFFER="tmux attach -t $(echo $selected_session | awk '{print $1}')"
-    zle accept-line
-    return
-  fi
-  if [[ ! -z $TMUX ]] && [[ -n "$selected_session" ]]; then # If session is selected run tmux
-    tmux switch-client -t "$(echo $selected_session | awk '{print $1}')"
-  fi
-}
+# Check if a session was selected
+if [[ -z $selected_session ]]; then
+  exit 0
+fi
 
-export -f tmux_session_switcher 2>/dev/null
+# Switch to the selected session
+if [[ -z $TMUX ]]; then # If not in tmux, attach to the selected session
+  tmux attach -t "$(echo "$selected_session" | awk '{print $1}')"
+else # If already in tmux, switch to the selected session
+  tmux switch-client -t "$(echo "$selected_session" | awk '{print $1}')"
+fi
--- script/tmux_sessionizer.sh
+++ script/tmux_sessionizer.sh
@@ -1,29 +1,52 @@
 #!/bin/bash
-tmux_sessionizer() {
-  if [[ $# -eq 1 ]]; then
-    selected=$1
-  else
-    selected=$(find ~ -type d | fzf --preview="exa -al {}")
-  fi
 
-  if [[ -z $selected ]]; then
-    return
-  fi
-
-  selected_name=$(basename "$selected" | tr . _)
-  tmux_running=$(pgrep tmux)
-
-  if [[ -z $TMUX ]] ; then
-    BUFFER="tmux new -s $selected_name -c $selected"
-    zle accept-line
-  else
-    if ! tmux has-session -t=$selected_name 2> /dev/null; then
-      tmux new-session -ds $selected_name -c $selected
-    fi
-
-    tmux switch-client -t $selected_name
-  fi
+# Function to check if a TMUX session exists
+session_exists() {
+  tmux has-session -t="$1" 2>/dev/null
 }
 
-export -f tmux_sessionizer 2>/dev/null
+# Function to create a new TMUX session
+create_session() {
+  tmux new-session -d -s "$1" -c "$2"
+}
 
+# Function to switch to an existing TMUX session
+switch_session() {
+  tmux switch-client -t "$1"
+}
+
+# Check if an argument is provided
+if [[ $# -eq 1 ]]; then
+  selected="$1"
+else
+  selected=$(find ~ -type d | fzf --preview="exa -al {}")
+fi
+
+# Check if selection is empty
+if [[ -z "$selected" ]]; then
+  exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+# Check if inside a TMUX session
+if [[ -z "$TMUX" ]]; then
+  # Check if a session with the same name already exists
+  if ! session_exists "$selected_name"; then
+    create_session "$selected_name" "$selected"
+  fi
+
+  tmux attach-session -t "$selected_name"
+else
+  # Check if the current session is the same as the selected session
+  current_session=$(tmux display-message -p '#S')
+  if [[ "$current_session" != "$selected_name" ]]; then
+    # Check if a session with the same name already exists
+    if ! session_exists "$selected_name"; then
+      create_session "$selected_name" "$selected"
+    fi
+
+    switch_session "$selected_name"
+  fi
+fi
