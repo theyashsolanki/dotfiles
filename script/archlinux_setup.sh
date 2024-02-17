ea7abe824d1969542d7ed0f07fd30fccd11e5c89
theyashsolanki <solankiyash556@gmail.com>
2024-02-17 14:18:17 +0530

    archlinux setup script

---
+++ script/archlinux_setup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+sudo pacman -Syu
+sudo pacman -S git neovim vim base-devel kitty firefox fzf i3 polybar rofi unzip tmux ripgrep tldr trash-cli eza lazygit bat npm picom brightnessctl feh nerd-fonts noto-fonts noto-fonts-emoji noto-fonts-cjk zsh zsh-syntax-highlighting zsh-autosuggestions zsh-history-substring-search bluez bluez-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth pavucontrol expac xclip polkit-gnome network-manager-applet go linux-zen-headers nvidia-dkms nvidia-utils
